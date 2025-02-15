52b535f7a62ed6b83adf932195103cd2f5b98030
Tsotne Kareli <cotneqareli123@gmail.com>
2025-02-15 10:25:58 +0400

    updated server url

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-  // baseURL: 'https://together-server-py.onrender.com',
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://together-server-py.onrender.com',
+  // baseURL: 'http://localhost:5000',
 
   headers: {
     'Content-Type': 'application/json',
